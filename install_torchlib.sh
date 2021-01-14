db28148c2d593292ef2443c232080925c6b435a4
Karl Kurzer <10877966+karlkurzer@users.noreply.github.com>
2021-01-14 11:47:24 +0100

    Update install_torchlib.sh

--- install_torchlib.sh
+++ install_torchlib.sh
@@ -2,6 +2,6 @@
 set -e
 
 wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.7.1%2Bcpu.zip
-mkdir -p /home/ws/$USER/.local
-unzip libtorch-*.zip -d /home/ws/$USER/.local
+mkdir -p /home/$USER/.local
+unzip libtorch-*.zip -d /home/$USER/.local
 rm libtorch-*.zip
