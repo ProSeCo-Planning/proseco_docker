2b3f24a0ff677b68a648869ec702952ecd9ab6d2
Timo K <tkleia34@gmail.com>
2021-04-01 15:45:39 +0200

    bump up torchlib version and fix unzip path

--- install_torchlib.sh
+++ install_torchlib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.7.1%2Bcpu.zip
+wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.8.1%2Bcpu.zip
 mkdir -p /home/$USER/.local
-unzip libtorch-*.zip -d /home/$USER/.local
+unzip libtorch-*.zip -d ~/.local
 rm libtorch-*.zip
