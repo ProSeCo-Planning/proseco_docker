dd871fc8958a1671071eab92db2501e820297554
Timo K <tkleia34@gmail.com>
2021-04-01 18:34:03 +0200

    remove superfluous mkdir

--- install_torchlib.sh
+++ install_torchlib.sh
@@ -2,6 +2,5 @@
 set -e
 
 wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.8.1%2Bcpu.zip
-mkdir -p /home/$USER/.local
 unzip libtorch-*.zip -d ~/.local
 rm libtorch-*.zip
