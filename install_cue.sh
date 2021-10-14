d4c6854578d005720e1809df4cacba2aa146607e
Karl Kurzer <10877966+karlkurzer@users.noreply.github.com>
2021-10-14 15:31:07 +0200

    Update install_cue.sh

--- install_cue.sh
+++ install_cue.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 wget -O cue.tar.gz https://github.com/cue-lang/cue/releases/download/v0.4.0/cue_v0.4.0_linux_amd64.tar.gz
-tar -zxvf cue.tar.gz -C /tmp
-cp /tmp/cue ~/.local/bin/
+mkdir -p ~/.local/bin/ && tar -zxvf cue.tar.gz -C ~/.local/bin/
 rm -rf cue.tar.gz
